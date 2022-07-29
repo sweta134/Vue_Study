6d201b8defa207ec03aa22c04163d9aa6e464efe
Sweta134 <upadhyaysweta17@gmail.com>
2022-07-29 12:20:10 +0530

    Adds iterating over array of arrays example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <h2 v-for="(name,index) in names" :key="name">{{index}} {{name}}</h2>
   <h2 v-for="name in fullNames" :key="name.first">{{name.first}} {{name.last}}</h2>
+
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{actor.name}}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
+  </div>
 </template>
 
 
@@ -15,6 +20,16 @@ export default {
         {first: 'Riley', last: 'Van'},
         {first: 'Sara', last: 'Lana'},
         {first: 'Clia', last: 'Lin'},
+      ],
+      actors:[
+        {
+          name: 'Christian Bale',
+          movies: ['Batman', 'The Prestige'],
+        },
+        {
+          name: 'Di Caprio',
+          movies: ['Titanic', 'Inception']
+        }
       ]
     };
   },
