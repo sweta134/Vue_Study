3b73123aa01688504731aaa84131de2ea6a4a3a6
Sweta134 <upadhyaysweta17@gmail.com>
2022-07-29 14:27:19 +0530

    Adds concepts in event handling part-2

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
  
   <h2>{{name}}</h2>
   <div>
-    <button v-on:mouseover="name='Batman'">Change name</button>
+    <button v-on:click="changeName($event), increment(1, $event)">Change name</button>
   </div>
   <h2>{{count}}</h2>
   <div>
-    <button v-on:click="increment(1)">Increment 1</button>
-    <button v-on:click="increment(5)">Increment 5</button>
-    <button v-on:click="decrement(1)">Decrement 1</button>
-    <button v-on:click="decrement(5)">Decrement 5</button>
+    <button @click="increment(1, $event)">Increment 1</button>
+    <button @click="increment(5)">Increment 5</button>
+    <button @click="decrement(1)">Decrement 1</button>
+    <button @click="decrement(5)">Decrement 5</button>
   </div>
 </template>
 
@@ -24,8 +24,13 @@ export default {
     };
   },
   methods:{
-    increment(num){
-      this.count+=num
+    changeName(event){
+      this.name="Batman";
+      console.log("Event",event);
+    },
+    increment(num, event){
+      this.count+=num;
+      console.log("Event",event);
     },
     decrement(num){
       this.count-=num
