665eac537d771c26fca90e99ccb04c3a835227d1
Sweta134 <upadhyaysweta17@gmail.com>
2022-07-29 14:11:10 +0530

    Example of using methods with and without parameters

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <h2 v-if="num === 0">The number is zero</h2>
-  <h2 v-else-if="num<0">The number is negative</h2>
-  <h2 v-else-if="num>0">The number is Positive</h2>
-  <h2 v-else>Not a number</h2>
-
-<template v-if="display ===true">
-<h2>Sweta</h2>
-  <h2>Codeevolution</h2>
-  <h2>Vue</h2>
-</template>
-
-<h2 v-show="showElement">using v-show</h2>
-<h2 v-if="showElement">using v-if</h2>
-  
+  <h2>{{2 + 3 + 5}}</h2>
+  <h2>{{5 + 10 + 15}}</h2>
+  <h2>Add method - {{add(2,3,5)}}</h2>
+  <h2>Add method - {{add(10,15,20)}}</h2>
+  <h2>Multiply method - {{multiply(baseValue)}}</h2>
 </template>
 
 <script>
@@ -21,11 +12,18 @@ export default {
   name: 'App',
   data(){
     return {
-      num: 'a',
-      display: true,
-      showElement: false
+      baseMultiplier: 5,
+      baseValue: 2
     };
   },
+  methods:{
+    add(a,b,c){
+      return a+b+c
+    },
+    multiply(num){
+      return num*this.baseMultiplier
+    }
+  }
 };
 </script>
 
