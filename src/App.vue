9364ee61b801f087473edbb45741006a278f3518
Sweta134 <upadhyaysweta17@gmail.com>
2022-07-29 11:56:03 +0530

    Adds topics conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num<0">The number is negative</h2>
+  <h2 v-else-if="num>0">The number is Positive</h2>
+  <h2 v-else>Not a number</h2>
+
+<template v-if="display ===true">
+<h2>Sweta</h2>
+  <h2>Codeevolution</h2>
+  <h2>Vue</h2>
+</template>
+
+<h2 v-show="showElement">using v-show</h2>
+<h2 v-if="showElement">using v-if</h2>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data(){
+    return {
+      num: 'a',
+      display: true,
+      showElement: false
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +37,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
 }
 </style>
